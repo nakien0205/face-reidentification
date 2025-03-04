1b0673d2454dd095edf1042850b22de7d8d4aefe
Nguyễn Trung Kiên <138596085+nakien0205@users.noreply.github.com>
2025-03-04 11:23:08 +0700

    Update FDM.py

--- liveness_detect/FDM.py
+++ liveness_detect/FDM.py
@@ -1,20 +1,21 @@
+
 import numpy as np
 import cv2
-import tensorflow as tf
 import keras
 # Load mô hình đã train
-model = keras.models.load_model("fake_image_detector.h5")
+model = keras.models.load_model(r"D:\Python\Projects\face-reidentification\liveness_detect\fake_image_detector.h5")
 
 # Hàm dự đoán ảnh
-def predict_image(image_path):
-    image = cv2.imread(image_path)
-    image = cv2.resize(image, (128, 128))  # Resize về kích thước phù hợp
-    image = image / 255.0  # Chuẩn hóa
-    image = np.expand_dims(image, axis=0)  # Thêm chiều batch
+def predict_image(image):
+    # If input is already a numpy array (cropped face)
+    if isinstance(image, np.ndarray):
+        # Resize the image
+        image = cv2.resize(image, (128, 128))
+        # Normalize
+        image = image / 255.0
+        # Add batch dimension
+        image = np.expand_dims(image, axis=0)
 
-    prediction = model.predict(image)
-    return "Ảnh giả" if prediction > 0.5 else "Ảnh thật"
-
-# Kiểm tra với một ảnh
-image_path = "face-reidentification/liveness_detect/dataset/train/real/LeNguyenQuocANh_AI1914 (9).jpg"
-print(predict_image(image_path))
+        prediction = model.predict(image)
+        print(prediction)
+        return "Fake" if prediction > 0.5 else "Real"
