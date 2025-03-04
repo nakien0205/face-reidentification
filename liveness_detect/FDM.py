4a6857c4a6c0c1a48dcdcd39bdedffa6b3198124
Nguyễn Trung Kiên <138596085+nakien0205@users.noreply.github.com>
2025-03-04 18:20:51 +0700

    Update FDM.py

--- liveness_detect/FDM.py
+++ liveness_detect/FDM.py
@@ -1,21 +1,15 @@
-
 import numpy as np
 import cv2
 import keras
 # Load mô hình đã train
-model = keras.models.load_model(r"D:\Python\Projects\face-reidentification\liveness_detect\fake_image_detector.h5")
+model = keras.models.load_model(r"D:\Python\Projects\face-reidentification\liveness_detect\model_1.h5")
 
 # Hàm dự đoán ảnh
 def predict_image(image):
-    # If input is already a numpy array (cropped face)
     if isinstance(image, np.ndarray):
-        # Resize the image
         image = cv2.resize(image, (128, 128))
-        # Normalize
         image = image / 255.0
-        # Add batch dimension
         image = np.expand_dims(image, axis=0)
 
         prediction = model.predict(image)
-        print(prediction)
-        return "Fake" if prediction > 0.5 else "Real"
+        return "Fake" if prediction < 0.5 else "Real"
